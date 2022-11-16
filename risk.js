38c443acfee1a1c54bab9ab41e533793766e33fc
lorenasandoval88 <lorenasandoval88@gmail.com>
2022-11-16 15:56:34 -0500

    removed Jonas code

--- risk.js
+++ risk.js
@@ -1,15 +1,29 @@
-console.log("risk.js loaded")
 
-risk={}
 
+console.log("risk.js loaded");
+
+// Get data from Rest API--------------
 cleanScores = ((dt) => {
-    dt = dt.split(/[\n\r]+/).filter(r=>r[0]!='#').map(r=>r.split(/\t/))
+    dt = dt.split(/[\n\r]+/).filter(r=>r[0]!='#').map(r=>r.split(/\t/));
+    return dt
 })
+const getPGSdata = async () => {
+    const entry = document.getElementById("inputbox").value
+    console.log(" Processing...");
+    const request = await fetch(`https://www.pgscatalog.org/rest/score/${entry}`);
+    const url = (await request.json()).ftp_scoring_file;
+    const range=[0,20000]
+    txt= pgs.pako.inflate(await (await fetch(url,{
+        headers:{
+            'content-type': 'multipart/byteranges',
+            'range': `bytes=${range.join('-')}`,
+        }
+    })).arrayBuffer(),{to:'string'})
+    //txt= cleanScores(txt).join(',')
+    return txt;
+};
 
-scores = async ()=> {
-    console.log('calling');
-    await pgs.loadScore(4);
-} 
-  
-sc=scores()
-
+getPGSdata().then(pgsData => {
+console.log("PGS data", pgsData);
+document.getElementById("pgsText").innerText =pgsData;
+});
