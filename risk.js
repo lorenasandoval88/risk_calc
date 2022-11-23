37517d3f2ef30451104df2604510b28d1c154711
lorenasandoval88 <lorenasandoval88@gmail.com>
2022-11-23 11:08:22 -0500

    op

--- risk.js
+++ risk.js
@@ -14,25 +14,25 @@ riskCalc = function() {
     indexArr = []
     riskArr = []
     risk.pgs_rsids.forEach((element,idx) => {
-        console.log(element)
-        console.log(risk.genom_rsids[idx])
+        //console.log(element)
+        //console.log(risk.genom_rsids[idx])
 
                  //get the index of the 23and me rsids that are also in PGS
                     var data_idx = risk.genom_rsids.indexOf(element)
                     if (data_idx!=-1){
                       indexArr.push(data_idx);
 
-                      var effect_allele = risk.genomDataArray[idx][2]
-                      var effect_allele_homozyg = effect_allele+effect_allele
-                      var data_alleles = risk.genomDataArray[data_idx][3]
-                      console.log("math.sum(risk)");
+                      var effect_allele = risk.pgsDataArray[idx][2]
+                      effect_allele_homozyg = effect_allele+effect_allele
+                      data_alleles = risk.genomDataArray[data_idx][3]
+                      //console.log(effect_allele);
 
                       if(effect_allele_homozyg==data_alleles){
-                        console.log("alleles match!", effect_allele_homozyg, "at ", risk.genomDataArray[idx][0])
-                                              //console.log("pgs row:",risk.genomDataArray[idx]); 
-                                              //console.log("23me row:",risk.genomDataArray[data_idx]); 
-                                              //console.log("pgs beta *2:",(risk.genomDataArray[idx][4])*2);
-                                              riskArr.push((risk.genomDataArray[idx][4])*2)
+                        //console.log("alleles match!", effect_allele_homozyg, "at ", risk.genomDataArray[idx][0])
+                                  //console.log("pgs row:",risk.genomDataArray[idx]); 
+                                  //console.log("23me row:",risk.genomDataArray[data_idx]); 
+                                  //console.log("pgs beta *2:",(risk.genomDataArray[idx][4])*2);
+                                  riskArr.push((risk.pgsDataArray[idx][4])*2)
                           }
                    }
                  })  
