f391ac3950e7fc4cbcb82337ba5d7ba8454ccf47
lorenasandoval88 <lorenasandoval88@gmail.com>
2022-12-06 18:59:17 -0500

    updated risk score and  posted number of PGS variants in 23andMe file

--- risk.js
+++ risk.js
@@ -13,9 +13,8 @@ riskCalc = function() {
     getVar2()
     indexArr = []
     riskArr = []
+    count = 1
     risk.pgs_rsids.forEach((element,pgs_idx) => { // skip first row column names (ie rsid)
-        //console.log(element)
-        //console.log(risk.genom_rsids[idx])
 
                  //get the index of the 23and me rsids that are also in PGS
                     var data_idx = risk.genom_rsids.indexOf(element)
@@ -25,36 +24,29 @@ riskCalc = function() {
                       var effect_allele = risk.pgsDataArray[pgs_idx][2]
                       effect_allele_homozyg = effect_allele+effect_allele
                       data_alleles = risk.genomDataArray[data_idx][3]
-                      console.log("effect_allele",effect_allele);
-                      console.log("data_alleles",data_alleles)
+                      // console.log("effect_allele",effect_allele);
+                      // console.log("data_alleles",data_alleles)
 
                       // find allele matches then seperatte homo v hetero matches
                       results = data_alleles.match(RegExp(effect_allele,'g'))
-                      console.log("results",results)
-                      //console.log("results length",results.length)
 
-                      console.log("data_idx",data_idx)
-                      console.log("pgs_idx",pgs_idx)
-
-                      console.log("risk.genom_rsids[data_idx]",risk.genom_rsids[data_idx])
-                      console.log("risk.pgsDataArray[pgs_idx][4]",risk.pgsDataArray[pgs_idx][4])
-
-                      console.log("element",element)
                       if(results!=null){
                         if( results.length==2){
                           riskArr.push((risk.pgsDataArray[pgs_idx][4])*2)
+                          count = count+1
                           }
                         else if(results.length==1){
                             riskArr.push((risk.pgsDataArray[pgs_idx][4])*1)
+                            count = count+1
                         }
                       }else{
-                        riskArr.push((risk.pgsDataArray[pgs_idx][4])*0)
+                        riskArr.push(0)
                         console.log("no match!",0)
+                        count = count+1
                       }
-                        // if(results.length==1){
-                        // riskArr.push((risk.pgsDataArray[pgs_idx][4])*1)
-                        // }
                    }
+                   variantMatches.innerHTML = `${riskArr.length+1} PGS variants found in 23andMe file`
+
                  })
   return math.sum(riskArr)
 }
